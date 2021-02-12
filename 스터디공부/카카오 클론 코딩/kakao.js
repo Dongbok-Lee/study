710e320c223908d3fb6e59fdabbd1440f54fc335
동복 <typoon0820@gmail.com>
2021-02-12 20:47:14 +0900

    second commit
    +카카오클론코딩 수정
    +사이트템플릿 추가

--- 스터디공부/카카오 클론 코딩/kakao.js
+++ 스터디공부/카카오 클론 코딩/kakao.js
@@ -1,8 +1,24 @@
-function mOver(i){
-  var t = document.querySelector(".use"+i);
-  if(t.style.display==='none'){
-    t.style.display = 'block';
-}else{
-  t.style.display = 'none';
+for (var i = 1; i < 7; i++) {
+  document.querySelector(".use" + i).style.display = "none";
 }
+
+function mOver(i) {
+  var t = document.querySelector(".use" + i);
+  for (var j = 1; j < 7; j++) {
+    if (i == j) {
+      j++;
+      if(j==7){
+        break;
+      }
+    }
+    if (document.querySelector(".use" + j).style.display == 'block') {
+      document.querySelector(".use" + j).style.display = 'none';
+    }
+  }
+
+  if (t.style.display == 'none') {
+    t.style.display = 'block';
+  }else{
+    t.style.display = 'none';
+  }
 }
