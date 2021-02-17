3bc6b8b7989c41abc52cf46ee277f6dec5c3ef8d
이동복 <typoon0820@gmail.com>
2021-02-17 10:56:54 +0900

    animation 20%

--- 스터디공부/프로젝트3-다양한 메뉴 애니메이션/animation.js
+++ 스터디공부/프로젝트3-다양한 메뉴 애니메이션/animation.js
@@ -1,37 +1,62 @@
 
 function move1(t){
   var Bar =document.querySelector("#u-Bar");
-  var List = document.querySelector('.menu-list'+t);
-  for(i=1; i<6; i++){
-    document.querySelector('.menu-list'+i).style.color='#b5b5b5';
-    List.onmouseover = function(){
-      List.style.color='#979797';
-    List.onmouseout =function(){
-        List.style.color='#b5b5b5';
-      }
-    }
-  }
   switch (t){
     case(1):
     Bar.style.transform='translate(435px,0px)';
-    List.style.color='#D94F5C';
+    fontcolor(1);
     break;
     case(2):
     Bar.style.transform='translate(522px,0px)';
-    List.style.color='#D94F5C';
+    fontcolor(2);
     break;
     case(3):
     Bar.style.transform='translate(622px,0px)';
-    List.style.color='#D94F5C';
+    fontcolor(3);
     break;
     case(4):
     Bar.style.transform='translate(720px,0px)';
-    List.style.color='#D94F5C';
+    fontcolor(4);
     break;
     case(5):
     Bar.style.transform='translate(815px,0px)';
-    List.style.color='#D94F5C';
-
+    fontcolor(5);
   }
     Bar.style.display='block';
+}
+function fontcolor(s){
+    var List = document.querySelector('#menu-list'+s);
+    for(var i=1; i<11; i++){
+      document.querySelector('#menu-list'+i).classList.replace('active1','hover');
+    }
+    List.classList.replace('hover','active1');
+    if(s>5&&s<11){
+      List.classList.add('active2')
+    }
+}
+function aniList(t){
+  var btn = document.querySelector('.ani_'+t);
+  for(var i = 1; i<7; i++){
+    document.querySelector('.ani_'+i).style.display='none';
+  }
+  btn.style.display='block';
+}
+function move2(t){
+  switch (t){
+    case(6):
+    fontcolor(6);
+    break;
+    case(7):
+    fontcolor(7);
+    break;
+    case(8):
+    fontcolor(8);
+    break;
+    case(9):
+    fontcolor(9);
+    break;
+    case(10):
+    fontcolor(10);
+  }
+
 }
